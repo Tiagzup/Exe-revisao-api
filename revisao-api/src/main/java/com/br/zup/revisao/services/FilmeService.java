6b80ceb25c8c6471c84c15fd47eae18a4ee939b7
Tiago Souza <tiago.souza@zup.com.br>
2019-08-01 23:03:33 -0300

    Implementar camada de serviço

--- revisao-api/src/main/java/com/br/zup/revisao/services/FilmeService.java
+++ revisao-api/src/main/java/com/br/zup/revisao/services/FilmeService.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.br.zup.revisao.exceptions.filmNotFoundException;
 import com.br.zup.revisao.modells.Filme;
 import com.br.zup.revisao.repositories.FilmeRepository;
 
@@ -16,6 +17,28 @@ public class FilmeService {
 	public Iterable<Filme> findAll() {
 		return filmeRepository.findAll();
 	}
+	public long quantityFilm () {
+		return filmeRepository.count();
+	}
+	public Filme catchFilmById (int id) {
+		return filmeRepository.findById(id).get();
+	}
+	public  void saveFilm (Filme filme) {
+		 filmeRepository.save(filme);
+	}
+	public void updateFilm (int id, Filme filme) {
+		Optional<Filme> optionalFilme = filmeRepository.findById(id);
+		if (!optionalFilme.isPresent()) {
+			
+		}
+	}
+	public void deleteFilm (int id) {
+		filmeRepository.deleteById(id); {
+			throw new filmNotFoundException("Não há filmes nesse id");
+		}
+		
+	}
+	
 	public void  rentFilm (int id, Filme filme) {
 		Optional<Filme> optionalFilme = filmeRepository.findById(id);
 		if(!optionalFilme.isPresent()) {
@@ -24,6 +47,7 @@ public class FilmeService {
 		filme.setQuantidadeDisponivel(filme.getQuantidadeDisponivel()-1);
 		filmeRepository.save(filme);
 	}
+	
 	
 	
 	
